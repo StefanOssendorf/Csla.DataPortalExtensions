d622f98bb8cc23ec701c891945b5cc71e9da40df
Stefan Ossendorf <stefan.ossendorf@googlemail.com>
2024-02-10 23:41:43 +0100

    Fix some namespace issues

--- tests/Csla.DataPortalExtensionGenerator.Tests.CsprojConfig/GeneratorConfigTests.cs
+++ tests/Csla.DataPortalExtensionGenerator.Tests.CsprojConfig/GeneratorConfigTests.cs
@@ -2,25 +2,27 @@ using Csla;
 
 namespace Ossendorf.Csla.DataPortalExtensionsGenerator.Tests.CsprojConfig;
 
-public class UnitTest1 {
+public class GeneratorConfigTests {
     [Fact]
     public async Task Test1() {
         IDataPortal<TestBO> tmp = null;
 
         if (tmp != null) {
-            await tmp.MyFetchAB(1);
+            await tmp.PrefixFetchABCSuffix(1);
         }
+
+        //Compilable test method is the test since I can't test the generated method in code :)
     }
 }
 
 [Serializable]
 public class TestBO : BusinessBase<TestBO> {
     [Fetch]
-    private Task FetchAB(int a) {
+    private Task FetchABC(int a) {
+        _ = a;
         return Task.CompletedTask;
     }
 }
-
 
 [Ossendorf.Csla.DataPortalExtensionGenerator.DataPortalExtensions]
 public static partial class TestExtensions {
