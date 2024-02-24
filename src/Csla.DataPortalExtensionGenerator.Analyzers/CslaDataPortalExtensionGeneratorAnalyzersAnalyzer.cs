17ade6b92bf2b1c007af4dff0460944cad64fc37
Stefan Ossendorf <stefan.ossendorf@googlemail.com>
2024-02-24 14:36:24 +0100

    Analyzer DPEG1000 implemented

--- src/Csla.DataPortalExtensionGenerator.Analyzers/CslaDataPortalExtensionGeneratorAnalyzersAnalyzer.cs
+++ src/Csla.DataPortalExtensionGenerator.Analyzers/CslaDataPortalExtensionGeneratorAnalyzersAnalyzer.cs
@@ -3,67 +3,71 @@ using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Operations;
 using System.Collections.Immutable;
 
-namespace Ossendorf.Csla.DataPortalExtensionGenerator.Analyzers {
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class NotDataPortalExtensionMethodUsedAnalyzer : DiagnosticAnalyzer {
-        public const string DiagnosticId = "DPEG1000";
+namespace Ossendorf.Csla.DataPortalExtensionGenerator.Analyzers;
 
-        private static readonly string Title = "Csla data portal method is used";
-        private static readonly string MessageFormat = "The data portal method '{0}' should not be used. Use a generated extension method instead.";
-        private static readonly string Description = "The data portal method should not be used. Instead the type and compile safe extension method from the data portal extension generator should be used.";
-        private const string Category = "Usage";
+/// <summary>
+/// Analyzer which reports the usage of the IDataPortal methods instead of the generated extension methods.
+/// </summary>
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class NotDataPortalExtensionMethodUsedAnalyzer : DiagnosticAnalyzer {
+    public const string DiagnosticId = "DPEG1000";
 
-        private static readonly DiagnosticDescriptor _rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+    private static readonly LocalizableString _title = new LocalizableResourceString(nameof(Resources.DPEG1000Title), Resources.ResourceManager, typeof(Resources));
+    private static readonly LocalizableString _messageFormat = new LocalizableResourceString(nameof(Resources.DPEG1000MessageFormat), Resources.ResourceManager, typeof(Resources));
+    private static readonly LocalizableString _description = new LocalizableResourceString(nameof(Resources.DPEG1000Description), Resources.ResourceManager, typeof(Resources));
+    private const string Category = "Usage";
 
-        /// <inheritdoc />
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_rule);
+    private static readonly DiagnosticDescriptor _rule = new(DiagnosticId, _title, _messageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: _description);
 
-        /// <inheritdoc />
-        public override void Initialize(AnalysisContext context) {
-            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
-            context.EnableConcurrentExecution();
+    /// <inheritdoc />
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_rule);
 
-            context.RegisterOperationAction(AnalyzeOperation, OperationKind.Invocation);
+    /// <inheritdoc />
+    public override void Initialize(AnalysisContext context) {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+
+        context.RegisterOperationAction(AnalyzeOperation, OperationKind.Invocation);
+    }
+
+    private void AnalyzeOperation(OperationAnalysisContext context) {
+        var invocationExpression = (IInvocationOperation)context.Operation;
+        var targetMethod = invocationExpression.TargetMethod;
+
+        var methodName = targetMethod.Name;
+
+        string expectedType;
+        switch (methodName) {
+            case "CreateAsync":
+            case "DeleteAsync":
+            case "ExecuteAsync":
+            case "FetchAsync":
+            case "UpdateAsync":
+                expectedType = "IDataPortal";
+                break;
+            case "CreateChildAsync":
+            case "FetchChildAsync":
+            case "UpdateChildAsync":
+                expectedType = "IChildDataPortal";
+                break;
+            default:
+                return;
         }
 
-        private void AnalyzeOperation(OperationAnalysisContext context) {
-            var targetMethod = ((IInvocationOperation)context.Operation).TargetMethod;
+        context.CancellationToken.ThrowIfCancellationRequested();
 
-            var methodName = targetMethod.Name;
-
-            string expectedType;
-            switch (methodName) {
-                case "CreateAsync":
-                case "DeleteAsync":
-                case "ExecuteAsync":
-                case "FetchAsync":
-                case "UpdateAsync":
-                    expectedType = "IDataPortal";
-                    break;
-                case "CreateChildAsync":
-                case "FetchChildAsync":
-                case "UpdateChildAsync":
-                    expectedType = "IChildDataPortal";
-                    break;
-                default:
-                    return;
-            }
-
-            context.CancellationToken.ThrowIfCancellationRequested();
-
-            if (targetMethod.ContainingType is not { ContainingNamespace.Name: "Csla" } cslaType) {
-                return;
-            }
-
-            context.CancellationToken.ThrowIfCancellationRequested();
-
-            if (cslaType.Name != expectedType) {
-                return;
-            }
-
-            context.CancellationToken.ThrowIfCancellationRequested();
-
-            context.ReportDiagnostic(Diagnostic.Create(_rule, targetMethod.Locations.First(), methodName));
+        if (targetMethod.ContainingType is not { ContainingNamespace.Name: "Csla" } cslaType) {
+            return;
         }
+
+        context.CancellationToken.ThrowIfCancellationRequested();
+
+        if (cslaType.Name != expectedType) {
+            return;
+        }
+
+        context.CancellationToken.ThrowIfCancellationRequested();
+
+        context.ReportDiagnostic(Diagnostic.Create(_rule, invocationExpression.Syntax.GetLocation(), methodName));
     }
 }
